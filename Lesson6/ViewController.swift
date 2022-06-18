cf8724299c9cda9e34430e9810b26a3874e705b7
mutanntix <gumus96@mail.ru>
2022-06-18 15:53:13 +0300

    Initial Commit

---
+++ Lesson6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson6
+//
+//  Created by Мурад Чеерчиев on 18.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
